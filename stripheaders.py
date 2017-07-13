836b6671ca81c3ce4ff4ffe885a71e6b61a53e7c
Mark Crovella, on laptop <crovella@cs.bu.edu>
2017-07-12 21:22:43 -0400

    toc debuggin

--- stripheaders.py
+++ stripheaders.py
@@ -1,11 +1,13 @@
 import sys
 
 outputLines = ['\section{', '\subsection{', '\subsubsection{', '\paragraph']
+printIt = False
 for line in sys.stdin:
     # correct for missing package that is used in typesetting tables
-    printIt = False
     for pattern in outputLines:
         if (line.find(pattern) > -1):
             printIt = True
     if printIt:
-        print(line)
+        print(line, end='')
+    if (line.find('}') > -1):
+        printIt = False
